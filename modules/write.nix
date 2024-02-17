d72ce514d993321f746a204a2cb5805f5eaa623e
Danila Danko <br4ch1st0chr0n3@gmail.com>
2024-02-17 17:54:19 +0300

    feat: write all workflows

--- modules/write.nix
+++ modules/write.nix
@@ -10,19 +10,31 @@
     };
   };
   config = {
-    write = lib.pipe config.workflows [
-      (lib.filterAttrs (name: value: value.path != null))
-      (lib.mapAttrs (name: value:
-        let
-          name' = (lib.strings.escapeNixIdentifier name);
-          generate = (pkgs.formats.yaml { }).generate name' config.clean.normalized.${name};
-          path = value.path;
-        in
-        pkgs.writeShellScriptBin name' ''
-          mkdir -p "$(dirname ${path})"
-          cp ${generate} "${path}"
-          chmod +w "${path}"
-        ''))
-    ];
+    write =
+      let
+        workflowWriters = lib.pipe config.workflows [
+          (lib.filterAttrs (name: value: value.path != null))
+          (lib.mapAttrs (name: value:
+            let
+              name' = (lib.strings.escapeNixIdentifier name);
+              generate = (pkgs.formats.yaml { }).generate name' config.clean.normalized.${name};
+              path = value.path;
+            in
+            pkgs.writeShellScriptBin name' ''
+              mkdir -p "$(dirname ${path})"
+              cp ${generate} "${path}"
+              chmod +w "${path}"
+            ''))
+        ]; in
+      workflowWriters
+      //
+      {
+        all = lib.pipe workflowWriters [
+          (lib.mapAttrsToList (_: value: lib.getExe value))
+          (lib.concatStringsSep "\n")
+          (pkgs.writeShellScriptBin "all")
+        ]
+        ;
+      };
   };
 }
