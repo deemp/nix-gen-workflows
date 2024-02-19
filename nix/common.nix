9bf4afbfa984e0f6e8be054dfb4e09c80991c01d
Danila Danko <br4ch1st0chr0n3@gmail.com>
2024-02-20 00:44:00 +0300

    refactor: remove old code

--- nix/common.nix
+++ nix/common.nix
@@ -18,27 +18,6 @@ rec
     };
 
     null_OrNullOrStr = null_Or (lib.types.nullOr lib.types.str);
-
-    # node = elemType: with lib; with lib.types; with lib.values; mkOptionType rec {
-    #   name = "node";
-    #   description = "node of type ${optionDescriptionPhrase (class: class == "noun" || class == "conjunction") elemType}";
-    #   descriptionClass = "conjunction";
-    #   check = x: x.type == "node" || elemType.check x.value;
-    #   merge = loc: defs:
-    #     let nrNulls = count (def: def.value == null_) defs; in
-    #     if nrNulls == builtins.length defs then null_
-    #     else if nrNulls != 0 then
-    #       throw "The option `${showOption loc}` is defined both null_ and not null_, in ${showFiles (getFiles defs)}."
-    #     else elemType.merge loc defs;
-    #   emptyValue = { value = { _type = "null"; }; };
-    #   getSubOptions = elemType.getSubOptions;
-    #   getSubModules = elemType.getSubModules;
-    #   substSubModules = m: node (elemType.substSubModules m);
-    #   functor = (defaultFunctor name) // { wrapped = elemType; };
-    #   nestedTypes.elemType = elemType;
-    # };
-
-    # node = lib.types.either null_OrNullOrStr;
   };
 
   options = rec {
